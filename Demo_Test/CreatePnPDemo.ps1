1dbdef5516516199f300cb6279203def9a779a57
thanhnh150500 <thanhnh150500@gmail.com>
2022-08-22 02:36:09 +0700

    Update CreatePnPDemo.ps1

--- Demo_Test/CreatePnPDemo.ps1
+++ Demo_Test/CreatePnPDemo.ps1
@@ -15,39 +15,3 @@ try {
 catch {
     Write-Host "Connect fail!" -f Red
 }
-
-$ListUrl = "pnp_testlist"
-$ListName = "PnpTest"
-#Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
-#Function to create a custom list
-Function Create-List($SiteURL, $ListName)
-{
-    #Set the Error Action
-    $ErrorActionPreference = "Stop"
- 
-    Try {
-        $Web = Get-SPWeb -Identity $SiteURL
-        $ListTemplate = [Microsoft.SharePoint.SPListTemplateType]::GenericList
-        
-        #Check if List with specific name exists
-        if($Web.Lists.TryGetList($ListName) -eq $null)
-        {
-            $List = $Web.Lists.Add($ListName, $ListName, $ListTemplate) 
-            write-host "List Created Successfully!" -ForegroundColor Green
-        }
-        else
-        {
-            write-host "List with a specific name already exists!" -ForegroundColor Red
-        }
-    }
-    catch {
-        Write-Host $_.Exception.Message -ForegroundColor Red
-    }
-    finally {
-        #Reset the Error Action to Default
-        $ErrorActionPreference = "Continue"
-    }
-}
- 
-#Call the function to create a new custom list
-Create-List $SiteURL $ListName
